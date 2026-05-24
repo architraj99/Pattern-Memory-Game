9c2ba38abe1fd0f762ba8fe685ffcb845d263a1c
architraj99 <rajarchit99@gmail.com>
2026-05-24 21:26:59 +0530

    Made basic game UI

---
+++ script.js
@@ -0,0 +1,11 @@
+let patternText = document.querySelector(".pattern-text");
+
+let startBtn = document.getElementById("startBtn");
+
+let optionsArea = document.querySelector(".options-area");
+
+let message = document.querySelector(".message");
+
+let score = document.querySelector(".score");
+
+let lives = document.querySelector(".lives");
