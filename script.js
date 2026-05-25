4b781f552fda960bb4c3e600d636c3454d99c3e9
architraj99 <rajarchit99@gmail.com>
2026-05-25 12:07:56 +0530

    Added timer, lives, game over and restart system

--- script.js
+++ script.js
@@ -23,19 +23,55 @@ let currentPattern = [];
 let playerSequence = [];
 let totalScore = 0;
 let totalLives = 3;
+let timeLeft = 10;
+let countdown;
 
 function shuffleArray(array) {
 
     for (let i = array.length - 1; i > 0; i--) {
 
         let randomIndex = Math.floor(Math.random() * (i + 1) );
-
         let temp = array[i];
         array[i] = array[randomIndex];
         array[randomIndex] = temp;
     }
 
     return array;
+}
+
+function startTimer() {
+
+    clearInterval(countdown);
+    timeLeft = 10;
+    let timerBox = document.querySelector(".timer");
+    timerBox.innerText = "Time: " + timeLeft;
+
+    countdown = setInterval(function () {
+
+            timeLeft--;
+            timerBox.innerText = "Time: " + timeLeft;
+
+            if (timeLeft <= 0) {
+
+                clearInterval(countdown);
+
+                totalLives--;
+                lives.innerText = "Lives: " + totalLives;
+                message.innerText = "Time Up";
+                message.style.color = "#dc2626";
+
+                checkGameOver();
+
+                if (totalLives > 0) {
+
+                    setTimeout(function () {
+
+                        generatePattern();
+                        showPattern();
+                    }, 1000);
+                }
+            }
+        }, 1000);
 }
 
 function generatePattern() {
@@ -61,15 +97,17 @@ function showPattern() {
     setTimeout(function () {
 
         patternText.style.visibility = "hidden";
+
         generateOptions();
+        startTimer();
     }, 3000);
 }
 
 function generateOptions() {
 
     optionsArea.innerHTML = "";
-
     let options = [...items];
+
     shuffleArray(options);
 
     options.forEach(function (item) {
@@ -79,13 +117,27 @@ function generateOptions() {
         optionBtn.classList.add("option-btn");
 
         optionBtn.addEventListener("click", function() {
-
+    
             handlePlayerChoice(item);
-            }
-        );
+        });
 
         optionsArea.appendChild(optionBtn);
     });
+}
+
+function checkGameOver() {
+
+    if(totalLives <= 0) {
+
+        clearInterval(countdown);
+
+        totalLives = 0;
+        lives.innerText = "Lives: 0";
+        message.innerText = "Game Over";
+        message.style.color = "#dc2626";
+        optionsArea.innerHTML = "";
+        startBtn.innerText = "Restart Game";
+    }
 }
 
 function handlePlayerChoice(item) {
@@ -93,27 +145,28 @@ function handlePlayerChoice(item) {
     playerSequence.push(item);
     let currentIndex = playerSequence.length - 1;
 
-    if (playerSequence[currentIndex] !== currentPattern[currentIndex]) {
+    if(playerSequence[currentIndex] !== currentPattern[currentIndex] ) {
 
         totalLives--;
-
         lives.innerText = "Lives: " + totalLives;
         message.innerText = "Wrong Pattern";
         message.style.color = "#dc2626";
         playerSequence = [];
-        
+
+        checkGameOver();
         return;
     }
 
     if (playerSequence.length === currentPattern.length) {
 
-        totalScore++;
+        clearInterval(countdown);
 
-        score.innerText = "Score: " + totalScore;
+        totalScore++;
+        score.innerText ="Score: " + totalScore;
         message.innerText = "Correct Pattern";
         message.style.color = "#16a34a";
 
-        setTimeout(function () {
+        setTimeout(function() {
 
             generatePattern();
             showPattern();
@@ -123,9 +176,15 @@ function handlePlayerChoice(item) {
 
 startBtn.addEventListener("click", function() {
 
+        if (totalLives <= 0) {
+            location.reload();
+            return;
+        }
+
         message.innerText = "Memorize the pattern";
         message.style.color = "#475569";
 
         generatePattern();
         showPattern();
-});
+    }
+);
